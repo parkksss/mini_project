a12fa1a00c8e7cfb80ca820018d74f7ae75da719
parkksss <yesleee9211@gmail.com>
2024-06-25 23:01:50 +0900

    [blahBlah] style: main 페이지 css 수정

--- blahBlah/components/GNB.tsx
+++ blahBlah/components/GNB.tsx
@@ -21,15 +21,22 @@ const GNB = function () {
 
     const authInitialized = loading || authUser === null;
 
-    return (<Box>
-        <Flex minH="60px" py={{ base: 2 }} px={{ base: 4 }} align="center" maxW="md" mx="auto">
-            <Spacer />
-            <Box flex="1">
-                <img style={{ height: '40px' }} src="/logo.svg" alt="logo" />
-            </Box>
-            <Box justifyContent="flex-end">{authInitialized ? loginBtn : logoutBtn}</Box>
-        </Flex>
-    </Box>)
+    return (
+        <Box
+            borderBottom={1}
+            borderStyle="solid"
+            borderColor='gray.200'
+            bg='white'
+        >
+            <Flex minH="60px" py={{ base: 2 }} px={{ base: 4 }} align="center" maxW="md" mx="auto">
+                <Spacer />
+                <Box flex="1">
+                    <img style={{ height: '40px' }} src="/logo.svg" alt="logo" />
+                </Box>
+                <Box justifyContent="flex-end">{authInitialized ? loginBtn : logoutBtn}</Box>
+            </Flex>
+        </Box>
+    );
 };
 
 export default GNB;
