02bdbbc08373c9350f5ca84a79d4e5855fb0a791
parkksss <yesleee9211@gmail.com>
2024-06-27 00:39:36 +0900

    [blahBlah] feat: 서버 에러클래스 생성하여 throw 처리

--- blahBlah/controllers/member.ctrl.ts
+++ blahBlah/controllers/member.ctrl.ts
@@ -1,14 +1,15 @@
 import MemberModel from "@/models/member/member.model";
 import { NextApiRequest, NextApiResponse } from "next";
+import BadReqError from "./error/bad_request_error";
 
 async function add(req: NextApiRequest, res: NextApiResponse) {
     const { uid, email, displayName, photoURL } = req.body;
     if (uid === undefined || uid === null) {
         // 에러 발생 : 에러를 만들어서 bad request 를 보내기
-        return res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
+        throw new BadReqError('uid가 누락되었습니다.');
     }
     if (email === undefined || email === null) {
-        return res.status(400).json({ result: false, message: 'email가 누락되었습니다.' });
+        throw new BadReqError('email가 누락되었습니다.')
     }
     const addResult = await MemberModel.add({ uid, email, displayName, photoURL });
     if (addResult.result === true) {
