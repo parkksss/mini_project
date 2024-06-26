195a2728ab4d47afdb4b9ba5a58ed1b6ce5939df
parkksss <yesleee9211@gmail.com>
2024-06-27 01:37:10 +0900

    [blahBlah] feat: 만들어진 handleError members.add.ts 에서 사용

--- blahBlah/pages/api/members.add.ts
+++ blahBlah/pages/api/members.add.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import CustomServerError from "@/controllers/error/custom_server_error";
+import handleError from "@/controllers/error/handle_error";
 import MemberCtrl from "@/controllers/member.ctrl";
-import MemberModel from "@/models/member/member.model";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -10,10 +11,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         if (supportMethod.indexOf(method!) === -1) {
             // 에러 반환
+            throw new CustomServerError({ statusCode: 400, message: '지원하지 않는 method' })
         }
         await MemberCtrl.add(req, res);
     } catch (err) {
         console.error(err);
         // 에러 처리
+        handleError(err, res);
     }
 }
