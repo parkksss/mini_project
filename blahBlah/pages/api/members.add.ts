39669659a9ebc1479e2aca5b9da71f34e8d54407
parkksss <yesleee9211@gmail.com>
2024-06-27 01:49:35 +0900

    [blahBlah] refactor: 공통으로 사용되는 메서드 체크 로직 함수로 만들기

--- blahBlah/pages/api/members.add.ts
+++ blahBlah/pages/api/members.add.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import CustomServerError from "@/controllers/error/custom_server_error";
+import checkSupportMethod from "@/controllers/error/check_support_method";
 import handleError from "@/controllers/error/handle_error";
 import MemberCtrl from "@/controllers/member.ctrl";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -9,10 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { method } = req;
     const supportMethod = ['POST'];
     try {
-        if (supportMethod.indexOf(method!) === -1) {
-            // 에러 반환
-            throw new CustomServerError({ statusCode: 400, message: '지원하지 않는 method' })
-        }
+        checkSupportMethod(supportMethod, method);
         await MemberCtrl.add(req, res);
     } catch (err) {
         console.error(err);
