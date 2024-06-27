8e9787059513d33b4abb16a79dfe28b0117ed45f
parkksss <yesleee9211@gmail.com>
2024-06-27 20:55:18 +0900

    [blahBlah] feat: 입력 컴포넌트 상단부 그리기

--- blahBlah/pages/[screenName]/index.tsx
+++ blahBlah/pages/[screenName]/index.tsx
@@ -1,6 +1,8 @@
 import { ServiceLayout } from "@/components/service_layout";
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { useState } from "react";
+import ResizeTextArea from 'react-textarea-autosize';
 
 const userInfo = {
     uid: 'test',
@@ -10,6 +12,8 @@ const userInfo = {
 }
 
 const UserHomePage: NextPage = () => {
+    const [message, setMessage] = useState('');
+    const toast = useToast();
     return (
         <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.50">
             <Box maxW="md" mx="auto" pt="6">
@@ -20,6 +24,43 @@ const UserHomePage: NextPage = () => {
                             <Text fontSize="md">{userInfo.displayName}</Text>
                             <Text fontSize="xs">{userInfo.email}</Text>
                         </Flex>
+                    </Flex>
+                </Box>
+                <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+                    <Flex align="center" p="2">
+                        <Avatar size="xs" src="https://bit.ly/brokken-link" mr="2" />
+                        <Textarea
+                            bg="gray.100"
+                            border="none"
+                            placeholder="무엇이 궁금한가요?"
+                            resize="none"
+                            minH="unset"
+                            overflow="hiddden"
+                            fontSize="xs"
+                            mr="2"
+                            maxRows={7}
+                            as={ResizeTextArea}
+                            value={message}
+                            onChange={(e) => {
+                                if (e.currentTarget.value) {
+                                    const lineCount = (e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 1) + 1;
+                                    if (lineCount > 7) {
+                                        toast({
+                                            title: '최대 7줄까지만 입력 가능합니다.',
+                                            position: 'top-right',
+                                        });
+                                        return;
+                                    } 
+                                }
+                                setMessage(e.currentTarget.value);
+                            }}/>
+                        <Button
+                            disabled={message.length === 0}
+                            bgColor="#FFBB6C"
+                            color="white"
+                            colorScheme="yelllow"
+                            variant="solid"
+                            size="sm">등록</Button>
                     </Flex>
                 </Box>
             </Box>
